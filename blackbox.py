4252bbedf61d7833d1988fb7c3c3a71562ef2aae
Dounia Lakhmiri <dounialakhmiri@gmail.com>
2020-11-20 14:39:54 -0500

    constrained problem

--- blackbox.py
+++ blackbox.py
@@ -6,8 +6,7 @@ import torch.backends.cudnn as cudnn
 import os
 import sys
 from evaluator import *
-from neural_net import *
-from resnet import *
+from neural_net2 import *
 from datahandler import DataHandler
 from thop import clever_format, profile
 
@@ -30,32 +29,35 @@ dataloader = DataHandler(dataset, batch_size)
 image_size, number_classes = dataloader.get_info_data
 trainloader, testloader = dataloader.get_loaders(resolution=resolution)
 #
-initial_image_size = 32
+initial_image_size = int(32*resolution)
 total_classes = 10
 number_input_channels = 3
 
 model = NeuralNet(depth, width)
-# model = resnet18()
-# model = NeuralNet(1.5, 1.5)
+# model = NeuralNet(1, 1)
 model.to(device)
 print(model)
 
-# optimizer = optim.RMSprop(model.parameters())
-optimizer = optim.Adam(model.parameters())
-# optimizer = optim.SGD(model.parameters(), lr=0.1, momentum=0.9, weight_decay=5e-4)
+optimizer = optim.SGD(model.parameters(), lr=0.1, momentum=0.9, weight_decay=5e-4)
+
 # The evaluator trains and tests the network
 evaluator = Evaluator(device, model, trainloader, testloader, optimizer, batch_size, dataset)
 print('> Training')
 # try:
 best_val_acc, best_epoch, nb_epochs = evaluator.train_and_test()
-print(best_val_acc)
+# print(best_val_acc)
 cnt = 1
-dsize = (1, 3, 32, 32)
+dsize = (1, 3, initial_image_size, initial_image_size)
 inputs = torch.randn(dsize).to(device)
 macs, params = profile(model, (inputs,), verbose=False)
 #
 # # Output of the blackbox
 print('> Final accuracy %.3f' % best_val_acc)
-print('Count eval', cnt)
-print('Number of epochs ', nb_epochs)
-print('MACS and NB_PARAMS', macs, params)
+
+# For ResNet18 with an image size of 32
+macs_baseline = 556651520
+flops_baseline = 11173962
+
+ratio_macs = macs / macs_baseline - 2.5
+ratio_flops = params / flops_baseline - 2.5
+print('MACS and FLOPS', ratio_macs, ratio_flops)
