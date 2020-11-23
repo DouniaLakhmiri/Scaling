9615e62cc9f6eb888ed7b62102f272f95ec4afd4
Dounia Lakhmiri <dounialakhmiri@gmail.com>
2020-11-23 11:49:45 -0500

    Fix bug
    The net needs the size of the image for the FC layer

--- blackbox.py
+++ blackbox.py
@@ -6,7 +6,7 @@ import torch.backends.cudnn as cudnn
 import os
 import sys
 from evaluator import *
-from neural_net import *
+from neural_net2 import *
 from datahandler import DataHandler
 from thop import clever_format, profile
 
@@ -28,12 +28,18 @@ batch_size = 256
 dataloader = DataHandler(dataset, batch_size)
 image_size, number_classes = dataloader.get_info_data
 trainloader, testloader = dataloader.get_loaders(resolution=resolution)
+
+# for batch in testloader:
+#     print(batch[0].size())
+#
+# for batch in trainloader:
+#     print(batch[0].size())
 #
 initial_image_size = int(32*resolution)
 total_classes = 10
 number_input_channels = 3
 
-model = NeuralNet(depth, width)
+model = NeuralNet(depth, width, initial_image_size)
 # model = NeuralNet(1, 1)
 model.to(device)
 print(model)
@@ -44,7 +50,7 @@ optimizer = optim.SGD(model.parameters(), lr=0.1, momentum=0.9, weight_decay=5e-
 evaluator = Evaluator(device, model, trainloader, testloader, optimizer, batch_size, dataset)
 print('> Training')
 # try:
-best_val_acc, best_epoch, nb_epochs = evaluator.train_and_test()
+# best_val_acc, best_epoch, nb_epochs = evaluator.train_and_test()
 # print(best_val_acc)
 cnt = 1
 dsize = (1, 3, initial_image_size, initial_image_size)
@@ -52,7 +58,7 @@ inputs = torch.randn(dsize).to(device)
 macs, params = profile(model, (inputs,), verbose=False)
 #
 # # Output of the blackbox
-print('> Final accuracy %.3f' % best_val_acc)
+# print('> Final accuracy %.3f' % best_val_acc)
 
 # For ResNet18 with an image size of 32
 macs_baseline = 556651520
